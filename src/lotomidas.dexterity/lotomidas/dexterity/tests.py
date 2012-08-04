c3ad5505cbead602bf03c32d91c010f7527b89c7
matheusldaraujo <0matheus.araujo0@gmail.com>
2012-08-04 13:54:32 -0300

    Add Content, Policy, Dexterity Products

---
+++ src/lotomidas.dexterity/lotomidas/dexterity/tests.py
@@ -0,0 +1,62 @@
+import doctest
+import unittest
+
+from Testing import ZopeTestCase as ztc
+
+from Products.Five import zcml
+from Products.PloneTestCase import PloneTestCase as ptc
+from Products.PloneTestCase.layer import PloneSite
+from Products.PloneTestCase.layer import onsetup
+
+import lotomidas.dexterity
+
+OPTION_FLAGS = doctest.NORMALIZE_WHITESPACE | \
+               doctest.ELLIPSIS
+
+ptc.setupPloneSite(products=['lotomidas.dexterity'])
+
+
+class TestCase(ptc.PloneTestCase):
+
+    class layer(PloneSite):
+
+        @classmethod
+        def setUp(cls):
+            zcml.load_config('configure.zcml',
+              lotomidas.dexterity)
+
+        @classmethod
+        def tearDown(cls):
+            pass
+
+
+def test_suite():
+    return unittest.TestSuite([
+
+        # Unit tests
+        #doctestunit.DocFileSuite(
+        #    'README.txt', package='lotomidas.dexterity',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+        #doctestunit.DocTestSuite(
+        #    module='lotomidas.dexterity.mymodule',
+        #    setUp=testing.setUp, tearDown=testing.tearDown),
+
+
+        # Integration tests that use PloneTestCase
+        ztc.ZopeDocFileSuite(
+            'INTEGRATION.txt', 
+            package='lotomidas.dexterity',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+        #ztc.FunctionalDocFileSuite(
+        #    'BROWSER.TXT',
+        #    package='lotomidas.dexterity',
+        #    optionflags = OPTION_FLAGS,
+        #    test_class=TestCase),
+
+        ])
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
